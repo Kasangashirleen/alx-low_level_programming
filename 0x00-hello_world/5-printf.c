b3d43437386ab964f85b17b0402d2a2d2c612dd0
root <you@example.com>
2022-12-30 07:10:06 -0800

    third commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,7 +3,7 @@
 /**
  * main - prints atatement with proper grammar
  * but statement is a piece of art
- * return statement return zero if code has no error
+ * Return: 0 if exited properly, non-zero otherwise
  */
 int main(void)
 {
