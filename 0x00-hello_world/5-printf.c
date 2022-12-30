18972c7e584ad1b5988dcf938951b7deaa98dbd8
root <you@example.com>
2022-12-30 07:07:19 -0800

    second commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
-int main(void){
+/**
+ * main - prints atatement with proper grammar
+ * but statement is a piece of art
+ * return statement return zero if code has no error
+ */
+int main(void)
+{
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return(0);
+	return (0);
 }
