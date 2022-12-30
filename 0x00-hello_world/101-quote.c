c2f50ea8e86cc77389059d5b78f3bff67cb5036a
root <you@example.com>
2022-12-30 07:31:56 -0800

    Second commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,8 +4,8 @@
 /**
  * main - prints "and that piece of art is useful"
  * - Dora Korpar, 2015-10-19",
- * followed by a new line, to the standard error.
- * Return- 0 if a success, otherwise it is an error
+ * followed by a new line,to the standard error.
+ * Return: 0 if a success,otherwise it is an error
  */
 int main(void)
 {	
