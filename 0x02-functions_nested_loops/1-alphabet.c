303270fa8686f20766280dc9f96604bfe0d5319c
root <you@example.com>
2022-12-31 08:48:04 -0800

    first commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print alphabet
+ * Return: 0 if code runs successfully
+ */
+
+void print_alphabet(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		_putchar(letter);
+		letter++;
+	}
+	_putchar('\n');
+}
